46c3645e5b32863e8f6da320289136a1ad4964cb
Peter M C Harrison <pmc.harrison@gmail.com>
2017-08-22 17:05:28 +0100

    Making a nice formatting template

--- functions/psych_UI.R
+++ functions/psych_UI.R
@@ -1,3 +1,9 @@
 shinyPsych_UI <- function(params) {
-  uiOutput("ui")
+  title_content <- wellPanel(h2(params$title, align = "center"))
+  main_content <- wellPanel(align = "center", uiOutput("ui"))
+  
+  fluidPage(fluidRow(column(12, title_content),
+    fluidRow(column(2),
+             column(8, main_content)),
+             column(2)))
 }
