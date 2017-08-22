9341410a484d257f337d59b765617066a00a389a
Peter M C Harrison <pmc.harrison@gmail.com>
2017-08-22 17:35:01 +0100

    minor changes

--- s4.R
+++ s4.R
@@ -7,28 +7,24 @@ setClass("app_logic", slots = list(pages = "list")
 
 setClass("test_element")
 
-setClass("test_page",
+setClass("page",
          slots = list(ui = "shiny.tag", # page UI
                       result = "character", # vector of results to save
                       triggers = "character", # inputs that trigger next page
                       final = "logical"), # whether page is final page or not
          contains = "test_element")
 
-# test_page_info_text shows a page with some text and 
+# one_btn_page shows a page with some text and 
 # a 'Next' button.
 setClass("one_btn_page",
          slots = list(body = "shiny.tag"),
-         contains = "test_page")
+         contains = "page")
 setMethod(
   f = "initialize",
   signature = "one_btn_page",
   definition = function(.Object, body) {
-    if (length(text) != 1) {
-      stop("Length of <text> slot must equal 1.")
-    }
     .Object@body <- body
-    .Object@ui <- div(body,
-                      actionButton("next", "Next"))
+    .Object@ui <- div(body, actionButton("next", "Next"))
     .Object@triggers <- "next"
     .Object@final <- FALSE
     return(.Object)
