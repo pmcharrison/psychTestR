222e8d3a01262dbcfea2119103824c27b8c95fb6
Peter M C Harrison <pmc.harrison@gmail.com>
2017-08-28 17:51:46 +0100

    Cosmetic changes

--- common_functions/psych_server.R
+++ common_functions/psych_server.R
@@ -24,28 +24,28 @@ initialiseRV <- function(params) {
 
 nextPage <- function(rv, input) {
   if (length(rv$test_stack) == 0) stop("No pages left to advance to!")
-  # Check validity of the current page, if appropriate
-  if (is(rv$current_page, "page") &&
-      .hasSlot(rv$current_page, "validate") &&
+  # Check validity of the current page. If validity check fails, quit
+  # the current operation.
+  if (.hasSlot(rv$current_page, "validate") &&
       !do.call(rv$current_page@validate, list(rv, input))) {
     return(FALSE) # i.e. we escape the nextPage evaluation, forcing input revision
   }
-  # If appropriate, finalise the current page
-  if (is(rv$current_page, "page") &&
-      .hasSlot(rv$current_page, "on_complete")) {
+  # Perform the <on_complete> function for the current page, if it exists.
+  if (.hasSlot(rv$current_page, "on_complete")) {
     do.call(rv$current_page@on_complete, list(rv, input))
   }
   # Deal with the next thing on the stack, whatever it is
   if (is(rv$test_stack[[1]], "code_block")) {
+    # Next thing on the stack is a code block.
+    # Code blocks are executed immediately, and then 
+    # we move to the next page.
     rv$current_page <- rv$test_stack[[1]]
-    print(rv$current_page)
-    fun <- rv$current_page@fun
     rv$test_stack <- rv$test_stack[- 1]
+    fun <- rv$current_page@fun
     do.call(fun, list(rv, input))
     nextPage(rv, input)
   } else if (is(rv$test_stack[[1]], "page")) {
     # Next thing on the stack is a test page
-    ## Move to the next page
     rv$current_page <- rv$test_stack[[1]]
     rv$test_stack <- rv$test_stack[- 1]
   } else {
