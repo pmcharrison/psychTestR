f30c0c062f2fb614a2f6c42b6039203cb6d4bbd4
Peter M C Harrison <pmc.harrison@gmail.com>
2017-10-29 21:35:49 +0100

    Adding Gold-MSI module

--- app.R
+++ app.R
@@ -1,17 +1,21 @@
-PIAT <- TRUE
+PIAT <- FALSE
+gold_MSI <- TRUE
 options(shiny.error = browser)
 if (PIAT) {
   source("tests/piat/load_piat.R")
-  server <- psychTestServer(params)
-  ui <- psychTestUI(params)
+} else if (gold_MSI) {
+  lapply(list.files("common_functions/", pattern = "*\\.R$", full.names = TRUE), source)
+  source("tests/gold_msi/gold_msi.R")
+  params <- new.env()
+  source("tests/gold_msi/params.R", local = params)
 } else {
   library(shinyBS)
   library(shinyWidgets)
   lapply(list.files("common_functions/", pattern = "*\\.R$", full.names = TRUE), source)
   params <- new.env()
   source("tests/cat/test_cat.R", local = params)
-  server <- psychTestServer(params)
-  ui <- psychTestUI(params)
 }
+server <- psychTestServer(params)
+ui <- psychTestUI(params)
 
 shinyApp(ui = ui, server = server)
--- tests/gold_msi/gold_msi.R
+++ tests/gold_msi/gold_msi.R
@@ -3,7 +3,7 @@ getMusicalTraining <- function() {
              body = tags$p("We will now ask you some questions about your musical background. You will be presented with some statements: your task is to say how much you agree with them."),
              on_complete = function(rv, input) {
                rv$results$musical_training <- list(detail = data.frame(),
-                                           total_score = 0)
+                                                   total_score = 0)
              })),
     lapply(list(
       list(q = "I have never been complimented for my talents as a musical performer.",
@@ -91,4 +91,135 @@ getMusicalTraining <- function() {
                 rv$results$musical_training$total_score + answer_num_weighted
             })
       }))
+}
+
+getGoldMSI <- function(sub_factors = "All", general_factor = FALSE,
+                       ask_best_instrument = FALSE) {
+  possible_sub_factors <- c("Active Engagement", "Emotions", "Musical Training",
+                            "Perceptual Abilities", "Singing Abilities")
+  csv <- read.csv("tests/gold_msi/items.csv", stringsAsFactors = FALSE)
+  
+  assertthat::assert_that(
+    is.character(sub_factors),
+    all(sub_factors %in% c(possible_sub_factors, "All")),
+    is.logical(general_factor),
+    assertthat::is.scalar(general_factor),
+    is.logical(ask_best_instrument), assertthat::is.scalar(ask_best_instrument),
+    all(possible_sub_factors %in% csv$factor_label),
+    all(csv$factor_label %in% possible_sub_factors)
+  )
+  
+  if ("All" %in% sub_factors) sub_factors <- possible_sub_factors
+  
+  items <- csv[csv$factor_label %in% sub_factors |
+                 (general_factor & csv$general_factor), ] %>%
+    cbind(., response = NA)
+  
+  assertthat::assert_that(
+    nrow(items) > 0
+  )
+  
+  prep_pages <- new("one_btn_page",
+                    body = tags$p("We will now ask you some questions about your musical background. You will be presented with some statements: your task is to say how much you agree with them."),
+                    on_complete = function(rv, input) {
+                      rv$results$gold_msi <- list(detail = items)
+                    })
+  
+  item_pages <- mapply(
+    function(position, question, 
+             btn1_text, btn2_text, btn3_text, btn4_text,
+             btn5_text, btn6_text, btn7_text) {
+      assertthat::assert_that(
+        is.numeric(position),
+        is.character(question),
+        assertthat::is.scalar(question),
+        is.character(btn1_text), assertthat::is.scalar(btn1_text),
+        is.character(btn2_text), assertthat::is.scalar(btn2_text),
+        is.character(btn3_text), assertthat::is.scalar(btn3_text),
+        is.character(btn4_text), assertthat::is.scalar(btn4_text),
+        is.character(btn5_text), assertthat::is.scalar(btn5_text),
+        is.character(btn6_text), assertthat::is.scalar(btn6_text),
+        is.character(btn7_text), assertthat::is.scalar(btn7_text)
+      )
+      new("page_NAFC",
+          prompt = tags$p(question),
+          response_options = as.character(1:7) %>% (function(x) {
+            names(x) <- c(btn1_text, btn2_text, btn3_text, btn4_text,
+                          btn5_text, btn6_text, btn7_text)
+            x
+          }),
+          on_complete = function(rv, input) {
+            rv$results$gold_msi$detail$response[position] <- 
+              as.numeric(input$lastBtnPressed)
+          })
+    },
+    seq_len(nrow(items)),
+    items$question,
+    items$btn1_text, items$btn2_text, items$btn3_text, items$btn4_text,
+    items$btn5_text, items$btn6_text, items$btn7_text
+  )
+  
+  best_instrument_page <- 
+    if (ask_best_instrument) {
+      new(
+        "page_dropdown",
+        prompt = tags$p("The instrument I play best (including voice) is ____"),
+        options = c("NA", "voice", "piano", "guitar", "drums", "xylophone",
+                    "flute", "oboe", "clarinet", "bassoon", "trumpet",
+                    "trombone", "tuba", "saxophone", "horn", "violin",
+                    "cello", "alto", "double bass", "harp") %>%
+          sapply(., Hmisc::capitalize, USE.NAMES = FALSE),
+        other_please_state = TRUE,
+        max_pixel_width = 200,
+        on_complete = function(rv, input) {
+          rv$results$gold_msi$best_instrument <- 
+            if (input$dropdown == "Other (please state)") {
+              input$other_please_state
+            } else {
+              input$dropdown
+            }
+        }
+      )
+    } else NULL
+  
+  final_pages <- new("one_btn_page",
+                     body = tags$p("You completed the musical background questionaire."),
+                     on_complete = function(rv, input) {
+                       rv$results$gold_msi$scores <- 
+                         scoreGoldMSI(rv$results$gold_msi$detail,
+                                      sub_factors = sub_factors,
+                                      possible_sub_factors = possible_sub_factors,
+                                      general_factor = general_factor)
+                     })
+  c(prep_pages, item_pages, best_instrument_page, final_pages)
+}
+
+scoreGoldMSI <- function(detail, sub_factors,
+                         possible_sub_factors,
+                         general_factor) {
+  assertthat::assert_that(
+    is.data.frame(detail),
+    is.character(sub_factors),
+    all(sub_factors %in% possible_sub_factors),
+    all(detail$factor_label %in% possible_sub_factors),
+    is.logical(general_factor), assertthat::is.scalar(general_factor)
+  )
+  sumScores <- function(df) {
+    ifelse(df$positive_scoring,
+           df$response,
+           8 - df$response) %>%
+      sum
+  }
+  res <- list()
+  for (sub_factor in sub_factors) {
+    res[[sub_factor]] <- 
+      detail[detail$factor_label == sub_factor, ] %>%
+      sumScores
+  }
+  if (general_factor) {
+    res$general <- 
+      detail[detail$general_factor, ] %>%
+      sumScores
+  }
+  res
 }
