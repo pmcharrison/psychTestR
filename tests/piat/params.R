5375d26973226be924656e48b85dfd253656ae0c
Peter M C Harrison <pmc.harrison@gmail.com>
2017-10-30 07:09:28 +0000

    1-item test version

--- tests/piat/params.R
+++ tests/piat/params.R
@@ -103,7 +103,7 @@ observeEvents <- function(rv, input, session) {
 }
 
 piat <- list()
-piat$items <- getStimuli()
+piat$items <- getStimuli()[1, ]
 
 test_modules <- list()
 test_modules$intro <- withTags(
