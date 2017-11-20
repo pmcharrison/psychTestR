04905b47298ee9b3e8e5b3a3e5a9fd68876eef97
Peter M C Harrison <pmc.harrison@gmail.com>
2017-11-20 17:43:54 +0100

    Enabling test closing

--- app.R
+++ app.R
@@ -1,3 +1,4 @@
+closed <- file.exists("closed.txt") # Make a text file called closed.txt to close the test
 PIAT <- TRUE
 gold_MSI <- FALSE
 options(shiny.error = browser)
@@ -6,23 +7,39 @@ if (Sys.info()["nodename"] == "Peters-MacBook-Pro.local") {
   options(shiny.error = browser) 
 }
 
-if (PIAT) {
-  source("tests/piat/load_piat.R")
-} else if (gold_MSI) {
-  lapply(list.files("common_functions/", pattern = "*\\.R$", full.names = TRUE), source)
-  source("tests/gold_msi/gold_msi.R")
-  params <- new.env()
-  source("tests/gold_msi/params.R", local = params)
+library(shiny)
+if (closed) {
+  shinyApp(
+    ui = fluidPage(
+      title = "Test closed",
+      div(
+        h2("Test closed"),
+        p("Sorry, this test is no longer available to participate in.")
+      )
+    ),
+    server = function(input, output, session) {
+      output$test <- renderText("Nothing")
+    }
+  )
 } else {
-  library(shinyBS)
-  library(shinyWidgets)
-  lapply(list.files("common_functions/", pattern = "*\\.R$", full.names = TRUE), source)
-  params <- new.env()
-  source("tests/cat/test_cat.R", local = params)
+  if (PIAT) {
+    source("tests/piat/load_piat.R")
+  } else if (gold_MSI) {
+    lapply(list.files("common_functions/", pattern = "*\\.R$", full.names = TRUE), source)
+    source("tests/gold_msi/gold_msi.R")
+    params <- new.env()
+    source("tests/gold_msi/params.R", local = params)
+  } else {
+    library(shinyBS)
+    library(shinyWidgets)
+    lapply(list.files("common_functions/", pattern = "*\\.R$", full.names = TRUE), source)
+    params <- new.env()
+    source("tests/cat/test_cat.R", local = params)
+  }
+  server <- psychTestServer(params)
+  ui <- psychTestUI(params)
+  
+  shinyApp(ui = ui, server = server,
+           onStart = function() {
+             onStop(function() params$server_quit_fun())})
 }
-server <- psychTestServer(params)
-ui <- psychTestUI(params)
-
-shinyApp(ui = ui, server = server,
-         onStart = function() {
-           onStop(function() params$server_quit_fun())})
