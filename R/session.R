41d2868456c607871809ab131f43b8e779c2eae6
Peter M C Harrison <pmc.harrison@gmail.com>
2018-04-18 18:22:07 +0100

    Neatening up session code

--- R/session.R
+++ R/session.R
@@ -2,11 +2,24 @@ manage_sessions <- function(state,
                             options,
                             session = shiny::getDefaultReactiveDomain()) {
   stopifnot(is.scalar.character(options$session_dir))
-  ssuid <- process_ssuid(state, session, session_dir = options$session_dir)
-  start_session(session, ssuid)
+  ssuid <- start_session(state, session, session_dir = options$session_dir)
   list(save_session(ssuid, state, session_dir = options$session_dir),
-       clean_session_dir(session = session,
-                         options = options))
+       clean_session_dir(session = session, options = options))
+}
+
+start_session <- function(state, session, session_dir) {
+  shiny::isolate({
+    ssuid = shiny::parseQueryString(session$clientData$url_search)$SSUID
+    if (is.null(ssuid)) {
+      ssuid <- get_new_ssuid(session_dir)
+    } else {
+      shinyjs::runjs("confirm_resume_session();")
+      load_successful <- load_session(state, ssuid, session_dir)
+      if (!load_successful) ssuid <- get_new_ssuid(session_dir)
+    }
+  })
+  session$sendCustomMessage("session_start", ssuid)
+  ssuid
 }
 
 get_new_ssuid <- function(session_dir) {
@@ -22,44 +35,6 @@ get_new_ssuid <- function(session_dir) {
     }
   }
   ssuid
-}
-
-save_timestamp <- function(session_dir, ssuid) {
-  assertthat::assert_that(
-    is.character(ssuid), assertthat::is.scalar(ssuid),
-    is.character(session_dir), assertthat::is.scalar(session_dir)
-  )
-  saveRDS(Sys.time(), file.path(session_dir, ssuid, "timestamp.RDS"))
-}
-
-#' Returns NA if no timestamp found
-read_timestamp <- function(session_dir, ssuid) {
-  assertthat::assert_that(
-    is.character(ssuid), assertthat::is.scalar(ssuid),
-    is.character(session_dir), assertthat::is.scalar(session_dir)
-  )
-  path <- file.path(session_dir, ssuid, "timestamp.RDS")
-  if (file.exists(path)) {
-    readRDS(path)
-  } else NA
-}
-
-process_ssuid <- function(state, session, session_dir) {
-  shiny::isolate({
-    ssuid = shiny::parseQueryString(session$clientData$url_search)$"SSUID"
-    if (is.null(ssuid)) {
-      ssuid <- get_new_ssuid(session_dir)
-    } else {
-      shinyjs::runjs("confirm_resume_session();")
-      load_successful <- load_session(state, ssuid, session_dir)
-      if (!load_successful) ssuid <- get_new_ssuid(session_dir)
-    }
-  })
-  ssuid
-}
-
-start_session <- function(session, ssuid) {
-  session$sendCustomMessage("session_start", ssuid)
 }
 
 save_session <- function(ssuid, state, session_dir) {
@@ -88,21 +63,30 @@ load_session <- function(state, ssuid, session_dir) {
   success
 }
 
+save_timestamp <- function(session_dir, ssuid) {
+  stopifnot(is.scalar.character(ssuid), is.scalar.character(session_dir))
+  saveRDS(Sys.time(), file.path(session_dir, ssuid, "timestamp.RDS"))
+}
+
+#' Returns NA if no timestamp found
+read_timestamp <- function(session_dir, ssuid) {
+  stopifnot(is.scalar.character(ssuid), is.scalar.character(session_dir))
+  path <- file.path(session_dir, ssuid, "timestamp.RDS")
+  if (file.exists(path)) readRDS(path) else NA
+}
+
 #' Deletes all session caches that are older than a certain amount.
 clean_session_dir <- function(session, options) {
-  session_dir <- options$session_dir
-  session_timeout_min <- options$session_timeout_min
-  clean_sessions_interval_min <- options$clean_sessions_interval_min
-  stopifnot(is.scalar.numeric(session_timeout_min),
-            is.scalar.numeric(clean_sessions_interval_min),
-            is.scalar.character(session_dir))
-  millis <- clean_sessions_interval_min * 60 * 1000
+  stopifnot(is.scalar.numeric(options$session_timeout_min),
+            is.scalar.numeric(options$clean_sessions_interval_min),
+            is.scalar.character(options$session_dir))
+  millis <- options$clean_sessions_interval_min * 60 * 1000
   shiny::observe({
     shiny::invalidateLater(millis, session)
-    dirs <- list.dirs(session_dir, recursive = FALSE)
+    dirs <- list.dirs(options$session_dir, recursive = FALSE)
     for (dir in dirs) {
       ssuid <- basename(dir)
       time <- read_timestamp(session_dir, ssuid)
-      if (!is.na(time) && time < Sys.time() - session_timeout_min * 60) {
+      if (!is.na(time) && time < Sys.time() - options$session_timeout_min * 60) {
         unlink(dir, recursive = TRUE)
       }}})}
