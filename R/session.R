8003ca593b96a0e4e7bf53124a1e6c5f27e7e813
Peter M C Harrison <pmc.harrison@gmail.com>
2018-04-19 18:36:48 +0100

    Refactoring

--- R/session.R
+++ R/session.R
@@ -1,29 +1,32 @@
 manage_sessions <- function(state,
                             options,
                             session = shiny::getDefaultReactiveDomain()) {
-  stopifnot(is.scalar.character(options$session_dir))
   if (options$enable_resume_session) {
     initialise_session(state, session, options)
-    list(save_session(state, session_dir = options$session_dir),
+    list(save_session(state, options = options),
          clean_session_dir(session = session, options = options))
   }
 }
 
 initialise_session <- function(state, session, options) {
   shiny::isolate({
-    p_id_url = shiny::parseQueryString(session$clientData$url_search)$p_id
+    p_id_url <- get_p_id_from_url(session)
     if (!is.null(p_id_url)) {
       try_resume_session(p_id = p_id_url, state, session, options,
                          ask_to_confirm_resume = TRUE,
                          reset_if_resume_fails = TRUE)
     } else {
-      # ... otherwise make a new p_id, if "auto" p_id mode is selected.
       if (options$auto_p_id) {
-        p_id(state) <- get_new_p_id(options$session_dir)
-        session$sendCustomMessage("push_p_id_to_url", p_id(state))
+        p_id <- generate_new_p_id(options$session_dir)
+        p_id(state) <- p_id
+        session$sendCustomMessage("push_p_id_to_url", p_id)
       }
     }
   })
+}
+
+get_p_id_from_url <- function(session) {
+  shiny::parseQueryString(session$clientData$url_search)$p_id
 }
 
 push_p_id_to_url <- function(p_id, session) {
@@ -38,7 +41,7 @@ try_resume_session <- function(p_id, state, session, options,
             is.scalar.logical(reset_if_resume_fails))
   p_id(state) <- p_id
   push_p_id_to_url(p_id, session)
-  data <- safe_load_session_data(p_id, options)
+  data <- safe_load_session(p_id, options)
   success <- !is.null(data)
   if (success) {
     if (ask_to_confirm_resume) {
@@ -52,67 +55,62 @@ try_resume_session <- function(p_id, state, session, options,
       shinyjs::alert(paste0("Couldn't find this user's testing session.\n",
                             "Beginning a new session."))
       shinyjs::runjs("reset_p_id_and_refresh_browser();")
+    } else {
+      shiny::showNotification("Starting new session.")
     }
   }
 }
 
-# reset_session <- function(state, retain_p_id = FALSE) {
-#   p_id <- p_id(state)
-#   initialise_state(state)
-#   if (retain_p_id) p_id(state) <- p_id
-#   invisible(TRUE)
-# }
-
-get_new_p_id <- function(session_dir) {
+generate_new_p_id <- function(options) {
   p_id <- NA
-  R.utils::mkdirs(session_dir)
+  R.utils::mkdirs(options$session_dir)
   while (is.na(p_id)) {
     candidate <- shiny:::createUniqueId(32)
-    is_unique <- dir.create(file.path(session_dir, candidate),
+    is_unique <- dir.create(file.path(options$session_dir, candidate),
                             showWarnings = FALSE)
     if (is_unique) {
       p_id <- candidate
-      save_timestamp(session_dir, p_id)
+      save_timestamp(options, p_id)
     }
   }
   p_id
 }
 
-save_session <- function(state, session_dir) {
+save_session <- function(state, options) {
   shiny::observe({
-    stopifnot(is(state, "state"), is.scalar.character(session_dir))
+    stopifnot(is(state, "state"), is.scalar.character(options$session_dir))
     p_id <- p_id(state)
     if (!is.null(p_id)) {
-      path.p_id <- file.path(session_dir, p_id)
+      path.p_id <- file.path(options$session_dir, p_id)
       path.data <- file.path(path.p_id, "data.RDS")
       R.utils::mkdirs(path.p_id)
       saveRDS(shiny::reactiveValuesToList(state), path.data)
-      save_timestamp(session_dir, p_id)
+      save_timestamp(options, p_id)
     }})
 }
 
-load_session_data <- function(p_id, options) {
+load_session <- function(p_id, options) {
   path <- file.path(options$session_dir, p_id, "data.RDS")
   readRDS(path)
 }
 
-safe_load_session_data <- function(p_id, options) {
+safe_load_session <- function(p_id, options) {
   res <- NULL
-  tryCatch(res <- load_session_data(p_id, options),
+  tryCatch(res <- load_session(p_id, options),
            warning = function(w) NULL,
            error = function(e) NULL)
   res
 }
 
-save_timestamp <- function(session_dir, p_id) {
-  stopifnot(is.scalar.character(p_id), is.scalar.character(session_dir))
-  saveRDS(Sys.time(), file.path(session_dir, p_id, "timestamp.RDS"))
+save_timestamp <- function(options, p_id) {
+  stopifnot(is.scalar.character(p_id), is.scalar.character(options$session_dir))
+  saveRDS(Sys.time(), file.path(options$session_dir, p_id, "timestamp.RDS"))
 }
 
 #' Returns NA if no timestamp found
-read_timestamp <- function(session_dir, p_id) {
-  stopifnot(is.scalar.character(p_id), is.scalar.character(session_dir))
-  path <- file.path(session_dir, p_id, "timestamp.RDS")
+read_timestamp <- function(options, p_id) {
+  stopifnot(is.scalar.character(p_id), is.scalar.character(options$session_dir))
+  path <- file.path(options$session_dir, p_id, "timestamp.RDS")
   if (file.exists(path)) readRDS(path) else NA
 }
 
