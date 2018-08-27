bdf133def1744ac7a07d1ed808cb8d9ce5648d11
Peter M C Harrison <pmc.harrison@gmail.com>
2018-08-27 17:09:29 +0100

    Introducing hash_df and unhash_df

---
+++ R/hash-df.R
@@ -0,0 +1,25 @@
+# Converts a data.frame into a hash table.
+hash_df <- function(x) {
+  if (!is.data.frame(x))
+    stop("input must be a dataframe ")
+  if (!is.character(x$key))
+    stop("input must have character column called 'key")
+  y <- new.env()
+  for (i in seq_len(nrow(x))) {
+    key <- x$key[i]
+    value <- as.list(x[i, ])
+    value$key <- NULL
+    y[[key]] <- value
+  }
+  y
+}
+
+# Converts a hash table, as created by hash_df(), into a data.frame.
+unhash_df <- function(x) {
+  y <- as.list(x)
+  keys <- names(y)
+  values <- lapply(y, function(x) as.data.frame(x, stringsAsFactors = FALSE)) %>%
+    do.call(rbind, .)
+  row.names(values) <- NULL
+  cbind(key = keys, values, stringsAsFactors = FALSE)
+}
---
+++ tests/testthat/test-hash_df.R
@@ -0,0 +1,16 @@
+context("hash_df")
+
+test_that("hash_df", {
+  csv <- data.frame(
+    key = c("A", "B", "C"),
+    GB = c("Hello", "Hi", "Goodbye"),
+    FR = c("Bonjour", "Salut", "Au revoir"),
+    stringsAsFactors = FALSE
+  )
+  expect_equal(csv, unhash_df(hash_df(csv)))
+  h <- hash_df(csv)
+  expect_equal(h$A$GB, "Hello")
+  expect_error(hash_csv(42))
+  expect_error(hash_csv(data.frame(Key = 10, GB = "Hi")))
+  expect_error(hash_csv(data.frame(key = 10, GB = "Hi")))
+}
