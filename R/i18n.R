cbf92d5c37f1c899bb207d4ee1227cd34e25ddd7
Peter M C Harrison <pmc.harrison@gmail.com>
2018-08-27 17:54:52 +0100

    added a hidden global variable called I18N_GLOBAL_DICT

--- R/i18n.R
+++ R/i18n.R
@@ -1,9 +1,9 @@
 #' @export
-i18_dict <- R6::R6Class(
-  "i18_dict",
+i18n_dict <- R6::R6Class(
+  "i18n_dict",
   public = list(
     initialize = function(x) {
-      i18_check(x)
+      i18n_check(x)
       private$dict <- hash_df(x)
     },
     as.data.frame = function() unhash_df(private$dict),
@@ -26,7 +26,7 @@ i18_dict <- R6::R6Class(
 )
 
 # Checks the validity of the dictionary
-i18_check <- function(x) {
+i18n_check <- function(x) {
   if (!is.data.frame(x))
     stop("input to i18n_dict() must be a dataframe ")
   if (!is.character(x$key))
@@ -36,3 +36,19 @@ i18_check <- function(x) {
   if (!all(sapply(x, is.character)))
     stop("all columns of input to i18n_dict() must be character class")
 }
+
+i18n_global_dict <- R6::R6Class(
+  "i18n_global_dict",
+  public = list(
+    initialize = function(x) private$val <- NULL,
+    get = function() private$val,
+    set = function(val) {
+      stopifnot(is(val, "i18n_dict"))
+      private$val <- val
+    },
+    reset = function() private$val <- NULL
+  ),
+  private = list(val = NULL)
+)
+
+I18N_GLOBAL_DICT <- i18n_global_dict$new()
--- tests/testthat/test-i18n.R
+++ tests/testthat/test-i18n.R
@@ -1,13 +1,13 @@
 context("i18n")
 
-test_that("i18_dict", {
+test_that("i18n_dict", {
   csv <- data.frame(
     key = c("A", "B", "C"),
     GB = c("Hello", "Hi", "Goodbye"),
     FR = c("Bonjour", "Salut", "Au revoir"),
     stringsAsFactors = FALSE
   )
-  x <- i18_dict$new(csv)
+  x <- i18n_dict$new(csv)
   expect_equal(x$as.data.frame(), csv)
   expect_equal(x$translate("A", "GB"), "Hello")
   expect_equal(x$translate("A", "FR"), "Bonjour")
@@ -16,4 +16,16 @@ test_that("i18_dict", {
   expect_equal(x$translate("test", "XXX", allow_missing = TRUE), "test")
 })
 
-
+test_that("I18N_GLOBAL_DICT", {
+  csv <- data.frame(
+    key = c("A", "B", "C"),
+    GB = c("Hello", "Hi", "Goodbye"),
+    FR = c("Bonjour", "Salut", "Au revoir"),
+    stringsAsFactors = FALSE
+  )
+  I18N_GLOBAL_DICT$reset()
+  I18N_GLOBAL_DICT$set(i18n_dict$new(csv))
+  expect_equal(I18N_GLOBAL_DICT$get()$as.data.frame(), csv)
+  I18N_GLOBAL_DICT$reset()
+  expect_equal(I18N_GLOBAL_DICT$get(), NULL)
+})
