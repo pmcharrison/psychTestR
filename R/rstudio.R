b88d406c9a2d7d301f84d7a9248d1e8ef12a4b77
Peter M C Harrison <pmc.harrison@gmail.com>
2018-09-08 08:49:13 +0100

    Adding two keyboard shortcuts

---
+++ R/rstudio.R
@@ -0,0 +1,17 @@
+insert_fun <- function(name) {
+  rstudioapi::insertText(paste0(name, "()"))
+  pos <- as.integer(rstudioapi::primary_selection(
+    x = rstudioapi::getSourceEditorContext()
+  )$range$start)
+  rstudioapi::setCursorPosition(
+    rstudioapi::document_position(pos[1], pos[2] - 1)
+  )
+}
+
+insert_i18n <- function() {
+  insert_fun("i18n")
+}
+
+insert_pt_i18n <- function() {
+  insert_fun("psychTestR::i18n")
+}
