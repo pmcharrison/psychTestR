1ca8acb4d546e52b4bee6415f5e23bc4c6c109b5
Peter M C Harrison <pmc.harrison@gmail.com>
2019-10-31 23:49:32 +0000

    Updated `order_at_run_time()` and `randomise_at_run_time()` so that they can be nested arbitrarily many times.

--- R/randomise-at-run-time.R
+++ R/randomise-at-run-time.R
@@ -3,6 +3,8 @@
 #' This helper function constructs a timeline where the order of
 #' test elements (or blocks of test elements) is determined at run time.
 #'
+#' @note This function can be nested arbitrarily many times.
+#'
 #' @inheritParams order_at_run_time
 #'
 #' @return
@@ -10,6 +12,35 @@
 #' with other test elements or timelines.
 #' These test elements will be presented in a freshly randomised
 #' order for each participant.
+#'
+#' @examples
+#' \donttest{
+#' c(
+#'   randomise_at_run_time(
+#'     "main",
+#'     list(
+#'       randomise_at_run_time(
+#'         "1",
+#'         list(one_button_page("1a"),
+#'              one_button_page("1b"),
+#'              one_button_page("1c"))
+#'       ),
+#'       randomise_at_run_time(
+#'         "2",
+#'         list(one_button_page("2a"),
+#'              one_button_page("2b"),
+#'              one_button_page("2c"))
+#'       ),
+#'       randomise_at_run_time(
+#'         "3",
+#'         list(one_button_page("3a"),
+#'              one_button_page("3b"),
+#'              one_button_page("3c"))
+#'       )
+#'     )
+#'   )
+#' ) %>% c(final_page("End")) %>% make_test()
+#' }
 #'
 #' @export
 randomise_at_run_time <- function(
