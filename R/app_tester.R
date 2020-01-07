28e00e306d0eda94f39de37420a04f3454027f92
Peter Harrison <pmc.harrison@gmail.com>
2020-01-07 14:51:41 +0000

    Documenting and exporting AppTester

--- R/app_tester.R
+++ R/app_tester.R
@@ -1,3 +1,76 @@
+#' App tester
+#'
+#' This is an R6 class for running automated evaluations of psychTestR tests
+#' using the \code{shinytest} framework.
+#' When you create an \code{AppTester} object,
+#' it spins up a Shiny app and a headless web browser,
+#' which you can then interact with by calling the methods of the
+#' \code{AppTester} object.
+#'
+#' - \code{app <- AppTester$new(dir)}:
+#' creates a new \code{AppTester} object. The argument
+#' \code{dir} should be a directory containing a file \code{app.R},
+#' with \code{app.R} containing your psychTestR test.
+#'
+#' - \code{app$stop()} - stops the daemon and deletes the output directory.
+#' Call this once you're finished with the \code{AppTester} object.
+#'
+#' - \code{app$click(buttons)} - clicks all the buttons with IDs
+#' listed in the character vector \code{buttons}.
+#'
+#' - \code{app$click_next()} - clicks the "Next" button.
+#'
+#' - \code{app$get_ui()} - returns the HTML of the current UI of the psychTestR test.
+#'
+#' - \code{app$get_ui_text()} - extracts displayed text from \code{app$get_ui()}
+#' using some heuristics. If \code{squish = TRUE}, extra whitespace
+#' is removed.
+#'
+#' - \code{app$expect_ui_text(text, squish = TRUE)} - throws an error
+#' if the output of \code{app$get_ui_text(squish = squish)}
+#' is not equal to the argument \code{text}.
+#'
+#' - \code{app$get_locals()} - returns the current local variables.
+#'
+#' - \code{app$get_globals()} - returns the current global variables.
+#'
+#' - \code{app$get_title()} - returns the current title.
+#'
+#' - \code{app$expect_title(text)} - throws an error
+#' if the output of \code{app$get_title()}
+#' is not equal to the argument \code{text}.
+#'
+#' - \code{app$get_problems_info()} - returns the "problems info"
+#' text displayed at the bottom of the page.
+#'
+#' - \code{app$expect_problems_info(text)} - throws an error
+#' if the output of \code{app$get_problems_info()}
+#' is not equal to the argument \code{text}.
+#'
+#' - \code{app$get_results()} - returns the current results for the current participant.
+#'
+#' - \code{app$expect_results(val)} - throws an error
+#' if the output of \code{as.list(app$get_results())}
+#' is not equal to \code{val}.
+#'
+#' - \code{app$clean_output()} - deletes the output directory.
+#'
+#' - \code{app$set_inputs(formA = "Text A", formB = "Text B")} -
+#' passes (for example) the string \code{"Text A"} to the UI
+#' input with ID \code{"formA"} and the string \code{"Text B"}
+#' to the UI input with ID \code{"formB"}.
+#'
+#' - \code{...} - \code{AppTester} objects inherit from \code{\link[shinytest]{ShinyDriver}}
+#' objects, and have access to all of their methods.
+#'
+#' @md
+#'
+#' @note
+#' \code{AppTester} depends on the prior installation of a headless web browser, PhantomJS,
+#' which you can install with \code{\link[shinytest]{installDependencies}}.
+#'
+#' @export
+#'
 AppTester <- R6::R6Class(
   "AppTester",
   inherit = shinytest::ShinyDriver,
