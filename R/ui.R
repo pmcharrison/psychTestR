c0c02de01d0e7262aa4dd17d41665988db302b52
sebsilas <sebsilas@gmail.com>
2023-01-03 09:19:52 +0100

    Gamify updates

--- R/ui.R
+++ R/ui.R
@@ -1,19 +1,15 @@
 ui <- function(opt) {
   header <- if (opt$display$show_header) {
     shiny::fluidRow(shiny::column(12, shiny::wellPanel(
+      id = "psychTestR_header",
       style = paste("padding: 3px; padding-left: 10px; padding-right: 10px;",
                     "background-color: white;"),
       shiny::div(
         style = paste("display: flex; justify-content: space-between;",
-                      "align-items: center;"),
+                      "align-items: center; text-align: left;"),
+        insert_logo(opt, 'left'),
         shiny::uiOutput("title"),
-        if (!is.null(opt$logo)) {
-          shiny::img(
-            src = opt$logo,
-            style = sprintf("width: %s; height: %s;",
-                            width = opt$logo_width,
-                            height = opt$logo_height))
-        } else " "
+        insert_logo(opt, 'right')
       )
     )))
   }
@@ -25,6 +21,7 @@ ui <- function(opt) {
       12L - opt$display$left_margin - opt$display$right_margin,
       style = "padding-left: 0px; padding-right: 0px;",
       shiny::wellPanel(
+        id = "psychTestR_content",
         align = "center",
         style = paste0(
           "background-color: ", opt$display$content_background_colour, "; ",
@@ -106,4 +103,25 @@ include_scripts <- function(opt) {
 }
 
 
+insert_logo <- function(opt, side) {
+
+  if (!is.null(opt$logo)) {
+    logo <- shiny::img(
+      id = "logo",
+      src = opt$logo,
+      style = sprintf("text-align: left; display: inline; width: %s; height: %s;",
+                      width = opt$logo_width,
+                      height = opt$logo_height))
+  } else {
+    logo <- " "
+  }
+
+  if(opt$logo_right & side == "right") {
+    logo
+  } else if(opt$logo_right == FALSE & side == "left") {
+    logo
+  } else {
+    " "
+  }
+}
 
