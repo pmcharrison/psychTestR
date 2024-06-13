390a6e1c0a824d6a3f382ca72b689242595a3533
sebsilas <sebsilas@gmail.com>
2024-06-14 00:49:03 +0200

    Alt approach

--- inst/js/get-ip.js
+++ inst/js/get-ip.js
@@ -1,5 +1,20 @@
+
+
+
+/*
 $(document).ready(function(){
   $.get("http://ipinfo.io", function(response) {
     Shiny.onInputChange("getIP", response);
   }, "json");
 });
+*/
+
+
+$(document).on("shiny:sessioninitialized",function(){$.get("https://api.ipify.org", function(response) {
+
+  console.log(response);
+  Shiny.setInputValue("getIP", response);
+
+
+});});
+
