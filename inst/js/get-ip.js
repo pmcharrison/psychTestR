be053901744740aee5beb789cbaafe59256d7301
sebsilas <sebsilas@gmail.com>
2024-06-14 01:05:46 +0200

    Only keep necessary JS

--- inst/js/get-ip.js
+++ inst/js/get-ip.js
@@ -1,15 +1,4 @@
 
-
-
-/*
-$(document).ready(function(){
-  $.get("http://ipinfo.io", function(response) {
-    Shiny.onInputChange("getIP", response);
-  }, "json");
-});
-*/
-
-
 $(document).on("shiny:sessioninitialized",function(){$.get("https://freeipapi.com/api/json", function(response) {
 
   console.log(response);
