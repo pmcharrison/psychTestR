ec23f8f4594b2645bf9b3c288b9aab5800cfb660
sebsilas <sebsilas@gmail.com>
2024-06-14 01:12:50 +0200

    Remove console log

--- inst/js/get-ip.js
+++ inst/js/get-ip.js
@@ -1,9 +1,6 @@
 
 $(document).on("shiny:sessioninitialized",function(){$.get("https://freeipapi.com/api/json", function(response) {
-
-  console.log(response);
   Shiny.setInputValue("getIP", response);
-
 
 });});
 
